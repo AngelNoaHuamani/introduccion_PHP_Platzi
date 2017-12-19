3303163a9311059d8d6b58ed1d566d0d7d5160e3
Angel Noa Huamani <angelnoahuamani@gmail.com>
2017-12-18 23:51:28 -0500

    funciones en PHP

---
+++ functions/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+/*function hello($name){
+	echo 'Hola '.$name;
+	echo '<br>';
+}
+
+hello('Caleb');
+hello('Angel');
+*/
+
+function suma($a , $b){
+	return $a + $b;
+}
+
+$c = suma(1, 2);
+var_dump($c);
+
+
+//Para insertar valores a una funcion se usan parametros.
+//Para "sacar" valores de una funcion se usa return.
+
+?>
